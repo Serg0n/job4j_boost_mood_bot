fe8f5133121ffeccfbdf844fe1919c0ad573d297
nevskysa-intelins-ru <cyppe135@gmail.com>
2025-06-16 16:59:36 +0300

    Внедрение коллекций | Поставщики контекста [#505300]

--- src/main/java/ru/job4j/bmb/engine/RecommendationEngine.java
+++ src/main/java/ru/job4j/bmb/engine/RecommendationEngine.java
@@ -1,16 +1,23 @@
 package ru.job4j.bmb.engine;
 
-import jakarta.annotation.PostConstruct;
-import jakarta.annotation.PreDestroy;
+import org.springframework.stereotype.Service;
+import ru.job4j.bmb.content.Content;
+import ru.job4j.bmb.content.ContentProvider;
 
+import java.util.List;
+import java.util.Random;
+
+@Service
 public class RecommendationEngine {
-    @PostConstruct
-    public void init() {
-        System.out.println("RecommendationEngine инициализирован");
+    private final List<ContentProvider> contents;
+    private static final Random RND = new Random(System.currentTimeMillis());
+
+    public RecommendationEngine(List<ContentProvider> contents) {
+        this.contents = contents;
     }
 
-    @PreDestroy
-    public void destroy() {
-        System.out.println("RecommendationEngine завершает работу");
+    public Content recommendFor(Long chatId, Long moodId) {
+        var index = RND.nextInt(0, contents.size());
+        return contents.get(index).byMood(chatId, moodId);
     }
 }
